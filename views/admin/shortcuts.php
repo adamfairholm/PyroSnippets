9d3f42b342caf7bc4f6780d1631f6d2bc561898b
clip <stephen.cozart@gmail.com>
2011-03-06 19:31:27 -0600

    removed sidebar and added shortcuts

---
+++ views/admin/shortcuts.php
@@ -0,0 +1,8 @@
+<nav id="shortcuts">
+	<h6><?php echo lang('cp_shortcuts_title')?></h6>
+	<ul>
+		<li><?php echo anchor('admin/chunks/create_chunk', lang('chunks.add_chunk')) ?></li>
+		<li><?php echo anchor('admin/chunks/list_chunks', lang('chunks.list_chunks')) ?></li>
+	</ul>
+	<br class="clear-both" />
+</nav>
