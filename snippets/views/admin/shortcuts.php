63c016b873df6feeb4de0ad3b4221c94bbc07ac9
Adam Fairholm <adam.fairholm@gmail.com>
2011-09-15 23:13:53 -0400

    Reversing shortcuts.

--- snippets/views/admin/shortcuts.php
+++ snippets/views/admin/shortcuts.php
@@ -1,8 +1,8 @@
 <nav id="shortcuts">
 	<h6><?php echo lang('cp_shortcuts_title')?></h6>
 	<ul>
-		<li><?php echo anchor('admin/snippets/create_snippet', lang('snippets.add_snippet'), array('class'=>'add')) ?></li>
 		<li><?php echo anchor('admin/snippets/list_snippets', lang('snippets.list_snippets'), array('class'=>'snippets')) ?></li>
+		<li><?php echo anchor('admin/snippets/create_snippet', lang('snippets.add_snippet'), array('class'=>'add')) ?></li>
 	</ul>
 	<br class="clear-both" />
 </nav>
