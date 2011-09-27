2414448d9012fe6be6feba980bc1767a7cef8afc
imamo <info@imamo.si>
2011-09-27 22:02:56 +0200

    Translation and code correction for translation
    I add slovenian translation and some code addition for full translation.

---
+++ snippets/language/slovenian/permission_lang.php
@@ -0,0 +1,9 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// Snippets Permissions
+$lang['snippets.role_admin_snippets'] = 'Administrator delčkov';
+
+
+
+/* End of file permission_lang.php */
+/* Location: ./addons/modules/snippets/language/slovenian/permission_lang.php */
