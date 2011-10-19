6c6897a6429f0f8bd70a01741e425ebdf6166ecf
Adam Fairholm <adam.fairholm@gmail.com>
2011-10-19 16:37:21 -0400

    Actions flush left.

--- snippets/views/admin/index.php
+++ snippets/views/admin/index.php
@@ -28,7 +28,7 @@
 							<td><?php echo $snippet->name; ?></td>
 							<td><?php echo $snippet_types[$snippet->type]; ?></td>
 							<?php if(group_has_role('snippets', 'admin_snippets')): ?><td>{pyro:snippet:<?php echo $snippet->slug; ?>}</td><?php endif; ?>
-							<td class="align-center buttons buttons-small">
+							<td class="actions">
 								<a href="<?php echo site_url('admin/snippets/edit_snippet/'.$snippet->id);?>" class="button edit"><?php echo lang('snippets.snippet_edit'); ?></a>
 								<?php if(group_has_role('snippets', 'admin_snippets')): ?><a href="<?php echo site_url('admin/snippets/delete_snippet/'.$snippet->id);?>" class="confirm button delete"><?php echo lang('snippets.snippet_delete'); ?></a><?php endif; ?>
 							</td>
