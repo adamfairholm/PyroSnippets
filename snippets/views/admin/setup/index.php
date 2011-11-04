d62fcb7f49e1b900418ff00bc7ff92648a12ef52
Adam Fairholm <adam.fairholm@gmail.com>
2011-11-04 15:23:54 -0400

    new lex syntax.

--- snippets/views/admin/setup/index.php
+++ snippets/views/admin/setup/index.php
@@ -27,7 +27,7 @@
 						<tr>
 							<td><?php echo $snippet->name; ?></td>
 							<td><?php echo $snippet_types[$snippet->type]; ?></td>
-							<td>{<?php echo config_item('tags_trigger'); ?>:snippet:<?php echo $snippet->slug; ?>}</td>
+							<td>{{ snippet:<?php echo $snippet->slug; ?> }}</td>
 							<td class="actions">
 								<a href="<?php echo site_url('admin/snippets/setup/edit_snippet/'.$snippet->id);?>" class="button edit"><?php echo lang('snippets.snippet_edit'); ?></a>
 								<?php if(group_has_role('snippets', 'admin_snippets')): ?><a href="<?php echo site_url('admin/snippets/setup/delete_snippet/'.$snippet->id);?>" class="confirm button delete"><?php echo lang('snippets.snippet_delete'); ?></a><?php endif; ?>
