3e8b961701a9e885361b57175cdc5895ce323bf0
Adam Fairholm <adam.fairholm@gmail.com>
2012-02-08 10:52:57 -0500

    Updating Slovenian language file from pull request file from @kubis just to make sure everything is there.

--- language/slovenian/snippets_lang.php
+++ language/slovenian/snippets_lang.php
@@ -1,7 +1,7 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
 /**
- * Snippets English Language File
+ * Snippets Slovenian Language File
  * 
  * @package		PyroCMS
  * @subpackage	PyroSnippets
@@ -9,45 +9,48 @@
  * @author		Danijel Ocepek - IMAMO (@danijeloc)
  */
 
-	$lang['snippets.name']									= 'Delčki strani';
+	$lang['snippets.name']								= 'Delčki strani';
 
 	// -------------------------------------
 	// Titles
 	// -------------------------------------
 
-	$lang['snippets.list_snippets']							= 'Seznam delčkov';
-	$lang['snippets.add_snippet']							= 'Dodaj delček';
-	$lang['snippets.edit_snippet']							= 'Uredi "%s" Delček';
-	$lang['snippets.setup']									= 'Namesti';
-	$lang['snippets.content']								= 'Vsebina';
-	$lang['snippets.syntax']								= 'Klicatelj';
+	$lang['snippets.list_snippets']						= 'Seznam delčkov';
+	$lang['snippets.add_snippet']						= 'Dodaj delček';
+	$lang['snippets.edit_snippet']						= 'Uredi "%s" Delček';
+	$lang['snippets.setup']								= 'Namesti';
+	$lang['snippets.content']							= 'Vsebina';
+	$lang['snippets.syntax']							= 'Klicatelj';
 
 	// -------------------------------------
 	// Messages
 	// -------------------------------------
 
-	$lang['snippets.no_snippets']							= 'Trenutno ne obstaja nobenega delčka strani.';
-	$lang['snippets.new_snippet_success']					= 'Delček je bil uspešno dodan';
-	$lang['snippets.new_snippet_error']						= 'Prišlo je do napake pri dodajanju delčka strani';
-	$lang['snippets.delete_snippet_success']				= 'Delček je bil uspešno odstranjen';
-	$lang['snippets.delete_snippet_error']					= 'Prišlo je do napake pri odstranjevanju delčka';
-	$lang['snippets.update_snippet_success']				= 'Delček je bil uspešno posodobljen';
-	$lang['snippets.update_snippet_error']					= 'Prišlo je do napake pri posodabljanju delčka';
+	$lang['snippets.no_snippets']						= 'Trenutno ne obstaja nobenega delčka strani.';
+	$lang['snippets.new_snippet_success']				= 'Delček je bil uspešno dodan';
+	$lang['snippets.new_snippet_error']					= 'Prišlo je do napake pri dodajanju delčka strani';
+	$lang['snippets.delete_snippet_success']			= 'Delček je bil uspešno odstranjen';
+	$lang['snippets.delete_snippet_error']				= 'Prišlo je do napake pri odstranjevanju delčka';
+	$lang['snippets.update_snippet_success']			= 'Delček je bil uspešno posodobljen';
+	$lang['snippets.update_snippet_error']				= 'Prišlo je do napake pri posodabljanju delčka';
 
 	// -------------------------------------
 	// Validation
 	// -------------------------------------
 
-	$lang['snippets.slug_unique']							= 'Povezava delčka mora bit unikatna.';
+	$lang['snippets.slug_unique']						= 'Povezava delčka mora bit unikatna.';
 
 	// -------------------------------------
 	// Form Tiles
 	// -------------------------------------
 
-	$lang['snippets.snippet_name']							= 'Naziv delčka';
-	$lang['snippets.snippet_slug']							= 'Povezava delčka';
-	$lang['snippets.snippet_type']							= 'Vrsta delčka';
-	$lang['snippets.snippet_content']						= 'Vsebina delčka';
+	$lang['snippets.snippet_name']						= 'Naziv delčka';
+	$lang['snippets.snippet_slug']						= 'Povezava delčka';
+	$lang['snippets.snippet_type']						= 'Vrsta delčka';
+	$lang['snippets.snippet_content']					= 'Vsebina delčka';
 	
-/* End of file snippets_lang.php */
-/* Location: ./addons/modules/snippets/language/slovenian/snippets_lang.php */
+		// -------------------------------------
+	// Snippet Parameters
+	// -------------------------------------
+	$lang['snippets.param.directory']					= 'Mapa s slikami';
+	$lang['snippets.param.allowed_types']				= 'Dovoljene vrste datotek (npr: png|jpg)';
