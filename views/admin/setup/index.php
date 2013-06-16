9f23765827dc575af1c5fb0530b8e73f96f7a96e
bitsketch <louis@bitsketch.com>
2013-06-16 15:57:41 -0500

    Corrected the Tag the user sees in their Snippets Admin Setup page
    New to snippets - couldn't understand why is wasn't working for me - it
    turns out the syntax was incorrect as shown in the admin panel.

--- views/admin/setup/index.php
+++ views/admin/setup/index.php
@@ -21,7 +21,7 @@
 				<tr>
 					<td><?php echo $snippet->name; ?></td>
 					<td><?php echo $snippet_types[$snippet->type]; ?></td>
-					<td>{{ snippet:<?php echo $snippet->slug; ?> }}</td>
+					<td>{{ snippets:<?php echo $snippet->slug; ?> }}</td>
 					<td class="actions">
 						<a href="<?php echo site_url('admin/snippets/setup/edit_snippet/'.$snippet->id);?>" class="button edit"><?php echo lang('global:edit'); ?></a>
 						<?php if(group_has_role('snippets', 'admin_snippets')): ?><a href="<?php echo site_url('admin/snippets/setup/delete_snippet/'.$snippet->id);?>" class="confirm button delete"><?php echo lang('global:delete'); ?></a><?php endif; ?>
